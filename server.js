7849355afb8c3fad1afcd69e4f7d8af1badf9d47
Step-Support <ttaywc08@gmail.com>
2024-11-03 00:23:48 -0700

    update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const compression = require('compression');
 const path = require('path');
 
+
 const server = express();
 
 // Enable gzip compression
@@ -14,5 +15,13 @@ server.use(express.static(path.join(__dirname, 'public')));
 server.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+const { inject } = require("@vercel/analytics");
+
+// Initialize Vercel Analytics
+inject();
+
+// Your other application code here
+console.log("Application is running with Vercel Analytics");
 
 module.exports = server;
